f360b6d393ca1163b99d2939304e89268f9694d9
Zheng <ahzzheng@gmail.com>
2021-03-26 11:07:36 -0700

    Add intersections, delete preview content

--- PathMapper/ContentView.swift
+++ PathMapper/ContentView.swift
@@ -8,6 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let intersections: [CGPoint] = [
+        CGPoint(x: 30, y: 50),
+        CGPoint(x: 370, y: 50),
+        
+        CGPoint(x: 30, y: 120),
+        CGPoint(x: 225, y: 120),
+        CGPoint(x: 370, y: 120),
+        
+        CGPoint(x: 30, y: 190),
+        CGPoint(x: 225, y: 190),
+        CGPoint(x: 370, y: 190),
+        
+        CGPoint(x: 30, y: 350),
+        CGPoint(x: 225, y: 350),
+        CGPoint(x: 370, y: 350)
+    ]
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
