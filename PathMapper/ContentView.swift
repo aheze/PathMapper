9c8bd76fca43083122662a04f20f3d0f84326d8e
Zheng <ahzzheng@gmail.com>
2021-03-26 12:13:05 -0700

    Add hallways

--- PathMapper/ContentView.swift
+++ PathMapper/ContentView.swift
@@ -26,6 +26,83 @@ struct ContentView: View {
         CGPoint(x: 370, y: 350)
     ]
     
+    struct DirectionalHallway {
+        var start: CGPoint
+        var end: CGPoint
+    }
+    
+    let hallways: [DirectionalHallway] = [
+        
+        /// Horizontal hallways
+        DirectionalHallway(
+            start: CGPoint(x: 30, y: 50),
+            end: CGPoint(x: 370, y: 50)
+        ),
+        
+        DirectionalHallway(
+            start: CGPoint(x: 370, y: 120),
+            end: CGPoint(x: 225, y: 120)
+        ),
+        DirectionalHallway(
+            start: CGPoint(x: 225, y: 120),
+            end: CGPoint(x: 30, y: 120)
+        ),
+        
+        DirectionalHallway(
+            start: CGPoint(x: 30, y: 190),
+            end: CGPoint(x: 225, y: 190)
+        ),
+        DirectionalHallway(
+            start: CGPoint(x: 225, y: 190),
+            end: CGPoint(x: 370, y: 190)
+        ),
+        
+        DirectionalHallway(
+            start: CGPoint(x: 370, y: 350),
+            end: CGPoint(x: 225, y: 350)
+        ),
+        DirectionalHallway(
+            start: CGPoint(x: 225, y: 350),
+            end: CGPoint(x: 30, y: 350)
+        ),
+        
+        /// Vertical hallways
+        DirectionalHallway(
+            start: CGPoint(x: 30, y: 120),
+            end: CGPoint(x: 30, y: 50)
+        ),
+        DirectionalHallway(
+            start: CGPoint(x: 30, y: 190),
+            end: CGPoint(x: 30, y: 120)
+        ),
+        DirectionalHallway(
+            start: CGPoint(x: 30, y: 350),
+            end: CGPoint(x: 30, y: 190)
+        ),
+        
+        DirectionalHallway(
+            start: CGPoint(x: 225, y: 190),
+            end: CGPoint(x: 225, y: 120)
+        ),
+        DirectionalHallway(
+            start: CGPoint(x: 225, y: 350),
+            end: CGPoint(x: 225, y: 190)
+        ),
+        
+        DirectionalHallway(
+            start: CGPoint(x: 370, y: 50),
+            end: CGPoint(x: 370, y: 120)
+        ),
+        DirectionalHallway(
+            start: CGPoint(x: 370, y: 120),
+            end: CGPoint(x: 370, y: 190)
+        ),
+        DirectionalHallway(
+            start: CGPoint(x: 370, y: 190),
+            end: CGPoint(x: 370, y: 350)
+        )
+    ]
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
