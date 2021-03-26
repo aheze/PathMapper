f71928e8b2ed5ee27b2cf7a943fa7eb77e2683a2
Zheng <ahzzheng@gmail.com>
2021-03-26 12:31:28 -0700

    Make code shorter, add classroom types

--- PathMapper/ContentView.swift
+++ PathMapper/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
+    /// hallway interesections
     let intersections: [CGPoint] = [
         CGPoint(x: 30, y: 50),
         CGPoint(x: 370, y: 50),
@@ -31,81 +32,54 @@ struct ContentView: View {
         var end: CGPoint
     }
     
+    struct Classroom {
+        var name: String
+        var buildingFrame: CGRect
+        var doorFrame: CGRect
+        var hallwayPoint: CGPoint
+    }
+    
+    struct MultiEntranceClassroom {
+        var name: String
+        var buildingFrame: CGRect
+        var doorFrames: [CGRect]
+        var hallwayPoints: [CGPoint]
+    }
+    
     let hallways: [DirectionalHallway] = [
         
-        /// Horizontal hallways
-        DirectionalHallway(
-            start: CGPoint(x: 30, y: 50),
-            end: CGPoint(x: 370, y: 50)
-        ),
+        /// horizontal hallways
+        DirectionalHallway( start: CGPoint(x: 30, y: 50), end: CGPoint(x: 370, y: 50) ),
         
-        DirectionalHallway(
-            start: CGPoint(x: 370, y: 120),
-            end: CGPoint(x: 225, y: 120)
-        ),
-        DirectionalHallway(
-            start: CGPoint(x: 225, y: 120),
-            end: CGPoint(x: 30, y: 120)
-        ),
+        DirectionalHallway( start: CGPoint(x: 370, y: 120), end: CGPoint(x: 225, y: 120) ),
+        DirectionalHallway( start: CGPoint(x: 225, y: 120), end: CGPoint(x: 30, y: 120) ),
         
-        DirectionalHallway(
-            start: CGPoint(x: 30, y: 190),
-            end: CGPoint(x: 225, y: 190)
-        ),
-        DirectionalHallway(
-            start: CGPoint(x: 225, y: 190),
-            end: CGPoint(x: 370, y: 190)
-        ),
+        DirectionalHallway( start: CGPoint(x: 30, y: 190), end: CGPoint(x: 225, y: 190) ),
+        DirectionalHallway( start: CGPoint(x: 225, y: 190), end: CGPoint(x: 370, y: 190) ),
         
-        DirectionalHallway(
-            start: CGPoint(x: 370, y: 350),
-            end: CGPoint(x: 225, y: 350)
-        ),
-        DirectionalHallway(
-            start: CGPoint(x: 225, y: 350),
-            end: CGPoint(x: 30, y: 350)
-        ),
+        DirectionalHallway( start: CGPoint(x: 370, y: 350), end: CGPoint(x: 225, y: 350) ),
+        DirectionalHallway( start: CGPoint(x: 225, y: 350), end: CGPoint(x: 30, y: 350) ),
         
-        /// Vertical hallways
-        DirectionalHallway(
-            start: CGPoint(x: 30, y: 120),
-            end: CGPoint(x: 30, y: 50)
-        ),
-        DirectionalHallway(
-            start: CGPoint(x: 30, y: 190),
-            end: CGPoint(x: 30, y: 120)
-        ),
-        DirectionalHallway(
-            start: CGPoint(x: 30, y: 350),
-            end: CGPoint(x: 30, y: 190)
-        ),
+        /// vertical hallways
+        DirectionalHallway( start: CGPoint(x: 30, y: 120), end: CGPoint(x: 30, y: 50) ),
+        DirectionalHallway( start: CGPoint(x: 30, y: 190), end: CGPoint(x: 30, y: 120) ),
+        DirectionalHallway( start: CGPoint(x: 30, y: 350), end: CGPoint(x: 30, y: 190) ),
         
-        DirectionalHallway(
-            start: CGPoint(x: 225, y: 190),
-            end: CGPoint(x: 225, y: 120)
-        ),
-        DirectionalHallway(
-            start: CGPoint(x: 225, y: 350),
-            end: CGPoint(x: 225, y: 190)
-        ),
+        DirectionalHallway( start: CGPoint(x: 225, y: 190), end: CGPoint(x: 225, y: 120) ),
+        DirectionalHallway( start: CGPoint(x: 225, y: 350), end: CGPoint(x: 225, y: 190) ),
         
-        DirectionalHallway(
-            start: CGPoint(x: 370, y: 50),
-            end: CGPoint(x: 370, y: 120)
-        ),
-        DirectionalHallway(
-            start: CGPoint(x: 370, y: 120),
-            end: CGPoint(x: 370, y: 190)
-        ),
-        DirectionalHallway(
-            start: CGPoint(x: 370, y: 190),
-            end: CGPoint(x: 370, y: 350)
-        )
+        DirectionalHallway( start: CGPoint(x: 370, y: 50), end: CGPoint(x: 370, y: 120) ),
+        DirectionalHallway( start: CGPoint(x: 370, y: 120), end: CGPoint(x: 370, y: 190) ),
+        DirectionalHallway( start: CGPoint(x: 370, y: 190), end: CGPoint(x: 370, y: 350) )
     ]
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Text("Hello, world!")
+                .background(Color.blue)
+                .position(x: 100, y: 100)
+            
+        }
     }
 }
 
---
+++ PathMapper/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  PathMapper
+//
+//  Created by Zheng on 3/26/21.
+//
+
+import SwiftUI
+//
+//struct MapView: View {
+//    
+//    var body: some View {
+//        
+//    }
+//}
+//
+//struct MapView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MapView()
+//    }
+//}
