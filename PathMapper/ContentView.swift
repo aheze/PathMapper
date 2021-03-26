50bc9e9a34f60b9e8cd85598237f177bab681f99
Zheng <ahzzheng@gmail.com>
2021-03-26 10:13:09 -0700

    Initial Commit

---
+++ PathMapper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PathMapper
+//
+//  Created by Zheng on 3/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PathMapper/PathMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathMapperApp.swift
+//  PathMapper
+//
+//  Created by Zheng on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct PathMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
